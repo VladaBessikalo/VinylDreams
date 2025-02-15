320f3989b33260ea207073c665b6ac139c040ce8
Vlada B <vlada.bessikalo@gmail.com>
2025-02-15 15:05:41 +0100

    chore: add firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyC-Bg_NW-CipZs_jug7Tp-bWNm_6e4MOOE',
+    authDomain: 'vinyldreams-vb.firebaseapp.com',
+    projectId: 'vinyldreams-vb',
+    storageBucket: 'vinyldreams-vb.firebasestorage.app',
+    messagingSenderId: '733838094369',
+    appId: '1:733838094369:web:4da956eb01d17a87af7360',
+    measurementId: 'G-V0P4HZWN3V'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
