86219b0d7d52a83894fb5f9164c32592d186569d
mikhalonok <bess.vlada@gmail.com>
2025-05-22 13:15:57 +0200

    fix: header options

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,91 +1,119 @@
-import { Link, useLocation } from 'react-router-dom';
-import { useAuth } from '../auth/context/AuthContext.jsx';
-import { AppBar, Toolbar, Typography, Box, Button } from '@mui/material';
-import HomeIcon from '@mui/icons-material/Home';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LoginIcon from '@mui/icons-material/Login';
-import LogoutIcon from '@mui/icons-material/Logout';
+import { Link, useLocation } from "react-router-dom";
+import { useAuth } from "../auth/context/AuthContext.jsx";
+import { AppBar, Toolbar, Typography, Box, Button } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LoginIcon from "@mui/icons-material/Login";
+import LogoutIcon from "@mui/icons-material/Logout";
 
-import SearchBar from './SearchBar.jsx';
-import vinylIcon from '../assets/romantic-vinyl-svgrepo-com.svg';
-import '../styles/Header.scss';
+import SearchBar from "./SearchBar.jsx";
+import vinylIcon from "../assets/romantic-vinyl-svgrepo-com.svg";
+import "../styles/Header.scss";
 
 export default function Header() {
-    const { user, logOut } = useAuth();
-    const { pathname } = useLocation();
+  const { user, logOut } = useAuth();
+  const { pathname } = useLocation();
 
-    return (
-        <Box>
-            <AppBar position="static" color="secondary" sx={{ paddingY: 1 }}>
-                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}}>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }} className="vinyl-logo">
-                        <Link to="/" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'inherit' }}>
-                            <img src={vinylIcon} alt="Vinyl Icon" style={{ width: 40, height: 40, marginRight: 8 }} className="vinyl-logo__vinyl-icon"/>
-                            <Typography variant="h6" sx={{ fontWeight: 700, fontStyle: "italic" }} className="vinyl-logo__text">
-                                Vinyl Dreams
-                            </Typography>
-                        </Link>
-                    </Box>
+  return (
+    <Box>
+      <AppBar position="static" color="secondary" sx={{ paddingY: 1 }}>
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            flexWrap: "wrap",
+          }}
+        >
+          <Box
+            sx={{ display: "flex", alignItems: "center" }}
+            className="vinyl-logo"
+          >
+            <Link
+              to="/"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                textDecoration: "none",
+                color: "inherit",
+              }}
+            >
+              <img
+                src={vinylIcon}
+                alt="Vinyl Icon"
+                style={{ width: 40, height: 40, marginRight: 8 }}
+                className="vinyl-logo__vinyl-icon"
+              />
+              <Typography
+                variant="h6"
+                sx={{ fontWeight: 700, fontStyle: "italic" }}
+                className="vinyl-logo__text"
+              >
+                Vinyl Dreams
+              </Typography>
+            </Link>
+          </Box>
 
-                    <Box sx={{ flexGrow: 1, maxWidth: 400, mx: 3 }}>
-                        <SearchBar />
-                    </Box>
+          <Box sx={{ flexGrow: 1, maxWidth: 400, mx: 3 }}>
+            <SearchBar />
+          </Box>
 
-                    <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-                        {user ? (
-                            <>
-                                {pathname === '/wishlist' ? (
-                                    <Link to="/" style={{ textDecoration: 'none' }}>
-                                        <Button
-                                            startIcon={<HomeIcon />}
-                                            variant="contained"
-                                            color="primary"
-                                        >
-                                            Choose new Vinyl Dreams
-                                        </Button>
-                                    </Link>
-                                ) : (
-                                    <Link to="/wishlist" style={{ textDecoration: 'none' }}>
-                                        <Button
-                                            startIcon={<FavoriteIcon color='secondary'/>}
-                                            variant="contained"
-                                            color="primary"
-                                        >
-                                            My Vinyl Dreams
-                                        </Button>
-                                    </Link>
-                                )}
-                                <Button
-                                    startIcon={<LogoutIcon />}
-                                    onClick={logOut}
-                                    variant="outlined"
-                                    color="inherit"
-                                >
-                                    Log out
-                                </Button>
-                            </>
-                        ) : (
-                            pathname === '/' && (
-                                <Link to="/login" style={{ textDecoration: 'none' }}>
-                                    <Button
-                                        startIcon={<LoginIcon />}
-                                        variant="outlined"
-                                        color="inherit"
-                                    >
-                                        Login
-                                    </Button>
-                                </Link>
-                            )
-                        )}
-                    </Box>
-                </Toolbar>
-            </AppBar>
+          <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+            {pathname === "/wishlist" ? (
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <Button
+                  startIcon={<HomeIcon />}
+                  variant="contained"
+                  color="primary"
+                >
+                  Choose new Vinyl Dreams
+                </Button>
+              </Link>
+            ) : (
+              <Link to="/wishlist" style={{ textDecoration: "none" }}>
+                <Button
+                  startIcon={<FavoriteIcon color="secondary" />}
+                  variant="contained"
+                  color="primary"
+                >
+                  My Vinyl Dreams
+                </Button>
+              </Link>
+            )}
 
-            <Box sx={{ textAlign: 'center', m: 2}}>
-                <Typography variant="subtitle1" color="secondary" fontStyle="italic" fontWeight="700">
-                    Vinyl Dreams, Tailored by You! Create your LPs Wish list here!
-                </Typography>
-            </Box>
-        </Box>
-    );
+            {user ? (
+              <Button
+                startIcon={<LogoutIcon />}
+                onClick={logOut}
+                variant="outlined"
+                color="inherit"
+              >
+                Log out
+              </Button>
+            ) : (
+              <Link to="/login" style={{ textDecoration: "none" }}>
+                <Button
+                  startIcon={<LoginIcon />}
+                  variant="outlined"
+                  color="inherit"
+                >
+                  Login
+                </Button>
+              </Link>
+            )}
+          </Box>
+        </Toolbar>
+      </AppBar>
+
+      <Box sx={{ textAlign: "center", m: 2 }}>
+        <Typography
+          variant="subtitle1"
+          color="secondary"
+          fontStyle="italic"
+          fontWeight="700"
+        >
+          Vinyl Dreams, Tailored by You! Create your LPs Wish list here!
+        </Typography>
+      </Box>
+    </Box>
+  );
 }
